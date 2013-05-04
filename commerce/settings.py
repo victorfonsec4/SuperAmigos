14ac500d3f4f0eba31ae64a8ebbb174ade99140f
Goku <victor19@gmail.com>
2013-05-04 10:24:53 -0300

    vaidamerda

--- commerce/settings.py
+++ commerce/settings.py
@@ -103,7 +103,7 @@ MIDDLEWARE_CLASSES = (
 'django.middleware.csrf.CsrfViewMiddleware',
 'django.contrib.auth.middleware.AuthenticationMiddleware',
 'django.contrib.messages.middleware.MessageMiddleware',
-'django-crossdomainxhr-middleware.py',
+'django-crossdomainxhr-middleware.XsSharing',
 # Uncomment the next line for simple clickjacking protection:
 # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
