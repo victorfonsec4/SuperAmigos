1531048bae4d9a180b6fb88636e5ef786e309ed1
Goku <victor19@gmail.com>
2013-05-03 21:27:22 -0300

    teste do database no home.html

--- commerce/views.py
+++ commerce/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from django.template import Context, loader
 from django.shortcuts import render
+from commerce.models import *
 
 def home(request):
-	return render(request, 'commerce/home.html')
+	produtoLista = Produto.objects.filter(id = 1)
+	produto = produtoLista[0]
+	context = {'produto':produto}
+	return render(request, 'commerce/home.html', context)
